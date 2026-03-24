1fa52f83f6f7a5f48b44fc854a303f8445796c12
sashaPshonko <privetnitron@gmail.com>
2026-03-24 13:11:38 +0500

    jjjjjjjk

--- 4narek112.mjs
+++ 4narek112.mjs
@@ -305,7 +305,7 @@ async function launchBookBuyer(name, password, anarchy) {
                             netakbistro = false;
                             await safeClickBuy(bot, slotToBuy, 1655, key);
                         } else if (slotToBuy < 9) {
-                            await safeClickBuy(bot, slotToBuy, getRandomDelayInRange(200, 250) * (slotToBuy + 2), key);
+                            await safeClickBuy(bot, slotToBuy, getRandomDelayInRange(300, 500) * (slotToBuy + 2), key);
                         } else {
                             await safeClickBuy(bot, slotToReloadAH, getRandomDelayInRange(1500, 4500), key);
                         }
