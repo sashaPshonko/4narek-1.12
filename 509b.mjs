56970609e564d1d6f33cdaf1be527ad9d9e1b136
sashaPshonko <privetnitron@gmail.com>
2026-05-05 10:37:54 +0500

    hhjhj

--- 509b.mjs
+++ 509b.mjs
@@ -12,7 +12,7 @@ const __dirname = dirname(__filename);
 
 // ========== КОНСТАНТЫ ==========
 const itemsPath = join(__dirname, 'items.json');
-const botsPath = join(__dirname, './bots/505b.json');
+const botsPath = join(__dirname, './bots/509b.json');
 const token = '8772352337:AAEattTfrJkFrkqes_xLQM-jXDEuCGju4Kc';
 const alertChatID = -1003827870631;
 const WEBSOCKET_URL = 'ws://85.198.86.42:8080/ws';
