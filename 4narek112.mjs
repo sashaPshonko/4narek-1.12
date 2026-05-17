4bb9d3d6b8222619b673f333ed30316409b57556
sashaPshonko <privetnitron@gmail.com>
2026-05-17 15:53:08 +0500

    lklkas

--- 4narek112.mjs
+++ 4narek112.mjs
@@ -1003,14 +1003,14 @@ async function safeAH(bot) {
     while (key === botKey) {
         await delay(1000);
 
-        const endTime = Date.now() + getRandomDelayInRange(4000, 5000);
+        const endTime = Date.now() + 5000;
 
         while (Date.now() < endTime) {
             const randomMove = ['forward', 'back', 'left', 'right'][Math.floor(Math.random() * 4)];
             bot.setControlState(randomMove, true);
-            await delay(getRandomDelayInRange(700, 1500));
+            await delay(getRandomDelayInRange(500, 1500));
             bot.setControlState(randomMove, false);
-            await delay(getRandomDelayInRange(500, 1300));
+            await delay(getRandomDelayInRange(500, 1000));
         }
         ['forward', 'back', 'left', 'right'].forEach(move => bot.setControlState(move, false));
         await delay(getRandomDelayInRange(1000, 2000))
@@ -1429,6 +1429,7 @@ async function walk(bot) {
     ['forward', 'back', 'left', 'right'].forEach(move => bot.setControlState(move, false));
 
 
+    bot.autoEat.disableAuto();
 }
 
 async function safeClickBuy(bot, slot, time, key) {
